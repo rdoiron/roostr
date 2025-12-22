10ac1a166243ccf4bfb605e365aff7e60520042b
rdoiron <rdoiron@gmail.com>
2025-12-22 17:18:45 -0500

    Fix container name in community app store exports.sh
    Update service references from roostr_app_1 to rdoiron-roostr_app_1
    to match the community app store naming convention.

--- rdoiron-roostr/exports.sh
+++ rdoiron-roostr/exports.sh
@@ -5,10 +5,10 @@
 # Other apps can use these to connect to the Roostr relay
 
 # Relay WebSocket URL for Nostr clients
-export APP_ROOSTR_RELAY_URL="ws://roostr_app_1:7000"
+export APP_ROOSTR_RELAY_URL="ws://rdoiron-roostr_app_1:7000"
 
 # Relay port for direct connections
 export APP_ROOSTR_RELAY_PORT="7000"
 
 # API endpoint for apps that want to interact with Roostr
-export APP_ROOSTR_API_URL="http://roostr_app_1:8080/api/v1"
+export APP_ROOSTR_API_URL="http://rdoiron-roostr_app_1:8080/api/v1"
